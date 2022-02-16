a02f2deacba128cebcf31334b965454861525bcc
SystemaRomanov <9663276@mail.ru>
2022-02-16 10:00:09 +0300

    макет с кнопками

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,16 @@ import 'package:flutter/material.dart';
     // This widget is the root of your application.
     @override
     Widget build(BuildContext context) {
+const borderStyle = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(36)),
+    borderSide: BorderSide(
+        color:const Color(0xFFbbbbbb), width: 2));
+const LinkTextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  color: Color(0xFF0079D0),
+);
+
       return MaterialApp(
         home: Scaffold(
             body: Container(
@@ -18,13 +28,17 @@ import 'package:flutter/material.dart';
                 SizedBox(height: 60,),
                 SizedBox(width: 110, height: 84, child: Placeholder(),),
                 SizedBox(height: 20,),
-                Text('Введите логин в виде 10 цифр номера телефона'),
+                Text('Введите логин в виде 10 цифр номера телефона',
+                style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 1)),
+            ),
                 SizedBox(height: 20,),
                 const SizedBox(width: 224,
                   child: TextField(
                     decoration: InputDecoration(
                       filled: true,
                       fillColor: Colors.grey,
+                      enabledBorder: borderStyle,
+                      focusedBorder: borderStyle,
                       labelText: 'Телефон',
                       ),
                     ),
@@ -36,19 +50,34 @@ import 'package:flutter/material.dart';
                     decoration: InputDecoration(
                       filled: true,
                       fillColor: Colors.grey,
+                      enabledBorder: borderStyle,
+                      focusedBorder: borderStyle,
                       labelText: 'Пароль',
                     ),
                   ),
                 ),
                 SizedBox(height: 20,),
                 SizedBox(width: 154,
-                    height: 42,
-                    child: ElevatedButton(
-                        onPressed: () {}, child: Text('Войти'))),
+                    height: 42, child:
+                    ElevatedButton(
+                        onPressed: () {},
+                      child: Text('Войти'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xFF0079D0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(36.0),
+                      ),
+                    ),
+                    )
+                ),
                 SizedBox(height: 20,),
-                InkWell(child: Text('Регистрация'), onTap: () {}),
+                InkWell(child: const Text('Регистрация',
+                style: LinkTextStyle,),
+                    onTap: () {}),
                 SizedBox(height: 20,),
-                InkWell(child: Text('Забыли пароль ?'), onTap: () {}
+                InkWell(child: const Text('Забыли пароль ?',
+                    style: LinkTextStyle,),
+                    onTap: () {}
                 ),
               ],
         ),
