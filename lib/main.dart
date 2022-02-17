153ac87687b6fe01f711aede8d01287bfef1a37b
SystemaRomanov <9663276@mail.ru>
2022-02-17 12:13:03 +0300

    макет с кнопками и с рисунками

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,30 @@ import 'package:flutter/material.dart';
   void main() {
   runApp(const MyApp());
   }
-
   class MyApp extends StatelessWidget {
     const MyApp({Key? key}) : super(key: key);
-
     // This widget is the root of your application.
     @override
     Widget build(BuildContext context) {
-const borderStyle = OutlineInputBorder(
+    const borderStyle = OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(36)),
     borderSide: BorderSide(
         color:const Color(0xFFbbbbbb), width: 2));
-const LinkTextStyle = TextStyle(
-  fontSize: 16,
-  fontWeight: FontWeight.bold,
-  color: Color(0xFF0079D0),
+    const  LinkTextStyle = TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 1)
 );
-
       return MaterialApp(
         home: Scaffold(
             body: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/bg1.jpg"),
+                fit: BoxFit.cover
+                )
+              ),
               width: double.infinity,
               child: Column(children: [
                 SizedBox(height: 60,),
-                SizedBox(width: 110, height: 84, child: Placeholder(),),
+                SizedBox(width: 110, height: 84, child: Image(image: AssetImage("assets/dart-logo.png"),),),
                 SizedBox(height: 20,),
                 Text('Введите логин в виде 10 цифр номера телефона',
                 style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 1)),
